b4994a419721585e53b9bd4eff35acf3188828a1
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2025-12-24 15:40:10 +0700

    fix

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -55,8 +55,9 @@ export const login = async (req, res) => {
         // Lưu token vào Cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict"
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            maxAge: 30 * 24 * 60 * 60 * 1000,
         }).status(200).json({
             message: "Đăng nhập thành công!",
             user: {
